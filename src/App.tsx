48a728b9d80e23fb62d0209e3f72ceff42bbb4d7
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-08 21:45:08 +0100

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+
 function App() {
-  return <h1>Hello</h1>
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Store />} />
+        <Route path="/" element={<About />} />
+      </Routes>
+    </Container>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
