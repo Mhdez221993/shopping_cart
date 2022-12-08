6e8b290d3f2f4df59d0af6888a42015e7e320b41
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-08 21:48:36 +0100

    Create Home, About & Store pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { About } from './pages/About'
 import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
 
 function App() {
   return (
     <Container>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Store />} />
-        <Route path="/" element={<About />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Container>
   )
---
+++ src/pages/Store.tsx
@@ -0,0 +1,3 @@
+export const Store = () => {
+  return <h1>Store</h1>
+}
