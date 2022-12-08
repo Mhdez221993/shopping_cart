8523c836853a36680e025d2bfd81989d0dd64c48
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-08 22:53:36 +0100

    Display images

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,13 @@
+import { Col, Row } from 'react-bootstrap'
+
+import storeItems from '../data/items.json'
+
 export const Store = () => {
-  return <h1>Store</h1>
+  return (
+    <Row md={2} xs={1} lg={3} className="g-3">
+      {storeItems.map((item) => (
+        <Col key={item.id}>{JSON.stringify(item)}</Col>
+      ))}
+    </Row>
+  )
 }
