d7d11abf36555555bfb5b5bbfb855f69bdcb44f6
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-09 20:33:26 +0100

    Display items img

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,7 +1,20 @@
+import { Card } from 'react-bootstrap'
+
 type ItemProps = {
   id: number
   name: string
   imgUrl: string
   price: number
 }
-export const Item = ({ id, name, imgUrl, price }: ItemProps) => {}
+export const Item = ({ id, name, imgUrl, price }: ItemProps) => {
+  return (
+    <Card>
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: 'cover' }}
+      />
+    </Card>
+  )
+}
