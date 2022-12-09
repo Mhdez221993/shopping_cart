90fd5c137a01fa1f435907cef768a7d2e90e2bff
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-09 22:35:34 +0100

    Create getItemQuantity method

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,18 +1,34 @@
-import { ReactNode, createContext, useContext } from 'react'
+import { ReactNode, createContext, useContext, useState } from 'react'
 
 type ShoppingCartProps = {
   children: ReactNode
 }
 
-type ShoppingCartContext = {}
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeFromCart: (id: number) => void
+}
 
-const ShoppingCartContext = createContext({})
+type CartItem = {
+  id: number
+  quantity: number
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
 export function useShppingCart() {
   return useContext(ShoppingCartContext)
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.quantity || 0
+  }
+
   return (
     <ShoppingCartContext.Provider value={{}}>
       {children}
