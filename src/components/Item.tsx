708d56e69c1a98f8ee7441a0d80f1a9b8a72e43f
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-09 20:48:16 +0100

    Add Card body

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -15,6 +15,13 @@ export const Item = ({ id, name, imgUrl, price }: ItemProps) => {
         height="200px"
         style={{ objectFit: 'cover' }}
       />
+
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   )
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,12 +5,15 @@ import storeItems from '../data/items.json'
 
 export const Store = () => {
   return (
-    <Row md={2} xs={1} lg={3} className="g-3">
-      {storeItems.map((item) => (
-        <Col key={item.id}>
-          <Item {...item} />
-        </Col>
-      ))}
-    </Row>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <Item {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
