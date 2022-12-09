60c6de75860da70481a315de9d9838a0629fad56
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-09 21:17:15 +0100

    Add increment , decrement & remove buttons

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,5 @@
-import { Card } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
+
 import { format_currency } from '../utilities/format_currency'
 
 type ItemProps = {
@@ -8,6 +9,8 @@ type ItemProps = {
   price: number
 }
 export const Item = ({ id, name, imgUrl, price }: ItemProps) => {
+  const quantity = 1
+
   return (
     <Card>
       <Card.Img
@@ -17,11 +20,35 @@ export const Item = ({ id, name, imgUrl, price }: ItemProps) => {
         style={{ objectFit: 'cover' }}
       />
 
-      <Card.Body className="d-flex flex-column">
+      <Card.Body className="d-flex flex-column w-100">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{format_currency(price)}</span>
         </Card.Title>
+
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: '.5rem' }}
+            >
+              <div
+                className="d-flex justify-content-center align-content-center"
+                style={{ gap: '.5rem' }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity} in cart</span>
+                </div>
+                <Button>+</Button>
+              </div>
+
+              <Button variant="danger">Remove</Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   )
