0110d82600fce9024ab34a5c5102c29b351ab47d
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-10 22:26:40 +0100

    apply open cart and import cart quantity

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
 
 import { NavLink } from 'react-router-dom'
+import { useShppingCart } from '../context/ShoppingCartContext'
 
 export const Navbar = () => {
+  const { cartQuantity, openCart, closeCart } = useShppingCart()
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
@@ -19,33 +21,36 @@ export const Navbar = () => {
             Store
           </Nav.Link>
         </Nav>
-        <Button
-          style={{ width: '3rem', height: '3rem', position: 'relative' }}
-          variant="outline-primary"
-          className="rounded-circle"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            style={{ width: '3rem', height: '3rem', position: 'relative' }}
+            variant="outline-primary"
+            className="rounded-circle"
           >
-            <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z" />
-          </svg>
-          <div
-            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-            style={{
-              color: 'white',
-              width: '1.5rem',
-              height: '1.5rem',
-              position: 'absolute',
-              bottom: 0,
-              right: 0,
-              transform: 'translate(25%, 25%)',
-            }}
-          >
-            3
-          </div>
-        </Button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z" />
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: 'white',
+                width: '1.5rem',
+                height: '1.5rem',
+                position: 'absolute',
+                bottom: 0,
+                right: 0,
+                transform: 'translate(25%, 25%)',
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
       </Container>
     </NavbarBs>
   )
